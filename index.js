0c007100d61d856b2f9c5d39f7b8a3dc051335b1
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-07-14 15:38:53 -0500

    adding changes: practicing with setInterval and setTimeout

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ boton.addEventListener('click', function () {
     console.log(this.innerHTML);
 
 })
+
+
+// var temporizador = setInterval(  function(){
+
+//             setColor();
+// }, 2000);
+
+// setTimeout( function(){
+//     setColor();
+// }, 5000);
+
+
+function setColor() {
+    var  pagina = document.body;
+    pagina.style.backgroundColor = pagina.style.backgroundColor == "blue" ? "green" : "blue";
+
+}
