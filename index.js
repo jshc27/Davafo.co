72834d9d9bfb260d1bb890853aaafd09262b6418
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-07-20 15:02:52 -0500

    modified the files, adding functions

---
+++ index.js
@@ -0,0 +1,10 @@
+function Capture() {
+
+    var nameComplete = document.getElementById("nameComplete").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    console.log(nameComplete);
+    console.log(email);
+    console.log(password);
+
+}
