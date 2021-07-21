71b30cae9cd415e5edfe34940461c1a3bbfdb6d1
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-07-21 17:40:22 -0500

    adding changes JS

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 "use strict"
 
-var getData = function() {
+var nameComplete = document.getElementById('nameComplete').value;
+var email = document.getElementById('email').value;
+var password = document.getElementById('password').value;
+var error = document.getElementById('error');
 
-    var nameComplete = document.getElementById('nameComplete').value;
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+
+function enviarForm(){
     console.log(nameComplete);
     console.log(email);
     console.log(password);
 
+    var mensajeError = [];
+
+    if(nameComplete.value === null || nameComplete.value === '') {
+        mensajeError.push('ingresa tu nombre completo');
+    }
+
+    if(email.value === null || email.value === '') {
+        mensajeError.push('ingresa tu email');
+    }
+
+    if(password.value === null || password.value === '') {
+        mensajeError.push('ingresa tu password');
+    }
+
+    error.innerHTML = mensajeError.join(', ');
+
+    return false;
 }
