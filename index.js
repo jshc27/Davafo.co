4fee47a87ec3e39318cd3a5372b1c099060ccad2
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-07-22 11:31:42 -0500

    adding changes: JS

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-"use strict"
 
 var nameComplete = document.getElementById('nameComplete');
 var email = document.getElementById('email').value;
@@ -6,24 +5,27 @@ var password = document.getElementById('password');
 var error = document.getElementById('error');
 
 
+
 function enviarForm(){
-    console.log('Nombre, email y password guardados');
+
+    console.log('guardando datos');
 
     var mensajeError = [];
 
     if(nameComplete.value === null || nameComplete.value === '') {
         mensajeError.push('ingresa tu nombre completo');
-    }
+    } 
 
     if(email.value === null || email.value === '') {
         mensajeError.push('ingresa tu email');
-    }
+    } 
 
     if(password.value === null || password.value === '') {
         mensajeError.push('ingresa tu password');
     }
 
-    error.innerHTML = mensajeError.join(', ');
+    error.innerHTML = mensajeError.join(' , ');
+    formulario.reset();
 
     return false;
 }
