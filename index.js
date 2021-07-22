c574b5a1cafd61cc2e90ad24af00e3f4e0114741
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-07-22 17:06:53 -0500

    adding changes: files, validations

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function validar(){
 
 
     let nameComplete = document.forms["formulario"]["nameComplete"].value;
+
     if(nameComplete == "") {
         alert("Ingresa tu nombre");
         return false;
@@ -14,12 +15,22 @@ function validar(){
         alert("Escribe tu nombre completo")
     }
 
-    let email = document.forms["formulario"]["nameComplete"].value;
-    if(nameComplete == "") {
-        alert("Ingresa tu nombre");
+    let email = document.forms["formulario"]["email"].value;
+    if(email == "") {
+        alert("Ingresa tu email");
         return false;
-    } else if (nameComplete.length<=6) {
-        alert("Escribe tu nombre completo")
+    } else if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email) ) {
+        alert("La dirección de email " + email + " es correcta!.");
+    } else {
+        alert("La dirección de email es incorrecta!.");
+    }
+
+    let password = document.forms["formulario"]["password"].value;
+    if(password == "") {
+        alert("Ingresa tu email");
+        return false;
+    } else if ( password.length<=8) {
+        alert("Ingresa mas de 8 cacteres");
     }
 
     console.log('guardando datos');
